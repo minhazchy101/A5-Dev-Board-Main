6cad082041d133bd4c3b2b0de9642c925eb82858
minhazchy101 <minhazchowdhury101@gmail.com>
2025-03-02 01:30:29 +0600

    complete button finished

--- javascript/style.js
+++ javascript/style.js
@@ -15,11 +15,70 @@ document.getElementById("color-btn").addEventListener(`click`, function(){
     document.body.style.backgroundColor = changeColor() ;
 })
 
+const navNumber = document.getElementById("nav-number");
+
+// 
+
 document.getElementById("discover-box").addEventListener("click", function () {
     
     window.location.href = "./qna.html";
 
 })
+
+// 
+
+const today = new Date();
+const theDate = today.toDateString();
+const currentDate = document.getElementById ("current-Date")
+
+currentDate.innerText = theDate;
+
+
+//
+
+const complete = document.querySelectorAll(".completed-btn");
+
+const taskNumber = document.getElementById("task-number");
+
+let buttons = 0;
+
+
+complete.forEach(function (complete) {
+    complete.addEventListener("click", function () {
+        
+        complete.setAttribute(`disabled`, true)
+        alert("Board update Successfully")
+        
+        
+        
+        
+        
+        let convertTaskNumber = parseInt(taskNumber.innerText);
+        
+    convertTaskNumber--;
+
+    taskNumber.innerText = convertTaskNumber;
+    
+           let convertNavNumber = parseInt(navNumber.innerText);
+
+           convertNavNumber++;
+
+           navNumber.innerText = convertNavNumber;
+
+           buttons++;
+           if (buttons === 6) {
+               alert("Congrates!! You have completed all the current task")
+           }
+
+
+    })
+    
+})
+
+
+
+
+
 
 
 
