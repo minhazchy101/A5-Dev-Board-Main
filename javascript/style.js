40603b852adcbde4fcac4eff14ce78922f0f221c
minhazchy101 <minhazchowdhury101@gmail.com>
2025-03-01 13:00:11 +0600

    first commit

---
+++ javascript/style.js
@@ -0,0 +1,22 @@
+// alert("okay")
+
+/*nav section*/ 
+
+function changeColor() {
+    const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+    return color;
+}
+
+document.getElementById("color-btn").addEventListener(`click`, function(){
+    document.body.style.backgroundColor = changeColor() ;
+})
+
+
+
+
+
+
